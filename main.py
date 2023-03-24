c2acdbe8170ce5b48f26881bbe046544c5505fdb
Peter Schiansky <peter.schiansky@univie.ac.at>
2023-03-24 14:56:22 +0100

    Create a plot of CO2/capita of Europe

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from warming.data.summary import Summary
 from warming.plot.maps import World
+from warming.plot.maps import Europe
+
 
 def main():
     summary = Summary()
     world = World()
     world.show(summary.co2(), "CO2C")
+    europe = Europe()
+    europe.show(summary.co2(), "CO2C")
 
 if __name__ == "__main__":
     main()
